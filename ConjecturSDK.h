effedc1d6d732c1a70345784b08dc6b67f9407d9
Random Bares <aeramor@gmail.com>
2013-11-22 12:14:04 -0800

    New version!
    Includes HTTPS directive
    Includes s_id parameter for security
    Includes API Key
    Moved variables to a PLIST to be easier to generate

--- ConjecturSDK.h
+++ ConjecturSDK.h
@@ -6,22 +6,10 @@
 //  Copyright (c) 2013 Random Bares. All rights reserved.
 //
 
-
-// Generated!
-#define kCampaignToken      @"conjecturExample"
-#define kApiKey             @"812893689126831"
-#define kAppID              @"9817286312"
-#define kAppName            @"ConjecturSDK"
-#define kAppVersion         [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"]
-#define kBundleID           [[NSBundle mainBundle] bundleIdentifier]
-
-
-
-
-
 @interface ConjecturSDK : NSObject
 
 + (BOOL)trackLaunch;
++ (BOOL)trackLaunch:(NSString*) custom;
 + (BOOL)trackUnlock:(NSURL*)url;
 
 @end
